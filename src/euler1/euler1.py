45490476518f69b4a60616d0b7392e27ad2fa6bb
Eduardo Stalinho <eduardooc.86@gmail.com>
2012-07-13 03:06:56 -0300

    the first solution in python

---
+++ src/euler1/euler1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ['sum_of_multiples']
+
+def sum_of_multiples():
+    '''
+    Find the sum of all the multiples of 3 or 5 below 1000.
+    '''
+    return sum([ x for x in range(1000) if ((x % 3 == 0) or (x % 5 == 0))])
---
+++ src/euler1/test_euler1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import unittest
+from problem1 import sum_of_multiples
+
+class TestSumOfMultiples(unittest.TestCase):
+
+    def test_sum_of_multiples(self):
+        '''
+        You are the 225183rd person to have solved this problem.
+        Let's get to the 233168!
+        '''
+        result = sum_of_multiples()
+        self.assertEqual(233168, result)
+
+if __name__ == '__main__':
+    unittest.main()
